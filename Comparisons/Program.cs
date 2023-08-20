3dc27c1de22003860288bc0ff05ee48f04967462
Jelena077 <jelenaasavicc@googlemail.com>
2023-08-20 22:04:10 +0200

    Add project files.

---
+++ Comparisons/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparisons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            int a = 20, b = 15;
+            result = (a > b);
+            Console.WriteLine("a greater than b: " + result);
+            result = (a < b);
+            Console.WriteLine("a lesser than b: " + result);
+            result = (a == b);
+            Console.WriteLine("a equal to b: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
